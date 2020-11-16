trozet/ovn-kubernetes

Clean up old local gw mode external ports
When we upgrade from 4.5->4.6 we transition from the old local gateway
mode to the new. In this case, OVS is clean when 4.6 comes up, but OVN
DB still has a leftover "br-local" port that exists on the external
switch. ovn-controller will try to wire this port to br-int, creating
double patch ports between br-ex and br-int, thus causing a broadcast
storm.

This patch ensures we remove the old port from OVN NB DB, which
ovn-controller will then ensure is removed from the external switch.

Signed-off-by: Tim Rozet <trozet@redhat.com>

go-controller/pkg/ovn/OCP_HACKS.go
@@ -5,6 +5,7 @@ import (
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/config"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/util"
 	kapi "k8s.io/api/core/v1"
+	"k8s.io/klog"
 	"strings"
 )
 
@@ -94,4 +95,27 @@ func gatewayInitMinimal(nodeName string, l3GatewayConfig *util.L3GatewayConfig, 
 		}
 	}
 	return nil
+}
+
+// cleanOldLocalGWPorts handles removing old "br-local" ports on external switches leftover from old local gw mode
+func cleanOldLocalGWPorts(nodeName string) {
+	oldLocalBridgeName := "br-local"
+	ifaceID := oldLocalBridgeName + "_" + nodeName
+	stdout, stderr, err := util.RunOVNNbctl("--data=bare", "--no-headings", "--columns=_uuid", "find",
+		"logical_switch_port", fmt.Sprintf("name=%s", ifaceID))
+	if err != nil {
+		klog.Errorf("Unable to query for old local gateway port: stderr: %s, error: %v", stderr, err)
+		return
+	}
+	if len(stdout) <= 0 {
+		return
+	}
+	klog.Infof("Found old local gateway interface to remove. Name: %s, ID: %s", ifaceID, stdout)
+	_, stderr, err = util.RunOVNNbctl("lsp-del", ifaceID)
+	if err != nil {
+		klog.Errorf("Unable to delete old local gateway interface: %s, stderr: %s, error: %v", ifaceID, stderr,
+			err)
+	} else {
+		klog.Infof("Old Local Gateway interface successfully removed: %s", ifaceID)
+	}
 }

go-controller/pkg/ovn/gateway_init.go
@@ -212,6 +212,10 @@ func gatewayInit(nodeName string, clusterIPSubnet []*net.IPNet, hostSubnets []*n
 			"stderr: %q, error: %v", externalSwitch, stdout, stderr, err)
 	}
 
+	// OCP HACK - clean up old local gateway ports that might still be in DB
+	cleanOldLocalGWPorts(nodeName)
+	// END OCP HACK
+
 	// Add external interface as a logical port to external_switch.
 	// This is a learning switch port with "unknown" address. The external
 	// world is accessed via this port.

alexanderConstantinescu/ovn-kubernetes-1

go-controller/pkg/ovn/OCP_HACKS.go
@@ -6,6 +6,7 @@ import (
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/types"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/util"
 	kapi "k8s.io/api/core/v1"
+	"k8s.io/klog"
 	"strings"
 )
 
@@ -95,4 +96,27 @@ func gatewayInitMinimal(nodeName string, l3GatewayConfig *util.L3GatewayConfig, 
 		}
 	}
 	return nil
+}
+
+// cleanOldLocalGWPort handles removing old "br-local" ports on external switches leftover from old local gw mode
+func cleanOldLocalGWPort(nodeName string) {
+	oldLocalBridgeName := "br-local"
+	ifaceID := oldLocalBridgeName + "_" + nodeName
+	stdout, stderr, err := util.RunOVNNbctl("--data=bare", "--no-headings", "--columns=_uuid", "find",
+		"logical_switch_port", fmt.Sprintf("name=%s", ifaceID))
+	if err != nil {
+		klog.Errorf("Unable to query for old local gateway port: stderr: %s, error: %v", stderr, err)
+		return
+	}
+	if len(stdout) == 0 {
+		return
+	}
+	klog.Infof("Found old local gateway interface to remove. Name: %s, ID: %s", ifaceID, stdout)
+	_, stderr, err = util.RunOVNNbctl("lsp-del", ifaceID)
+	if err != nil {
+		klog.Errorf("Unable to delete old local gateway interface: %s, stderr: %s, error: %v", ifaceID, stderr,
+			err)
+	} else {
+		klog.Infof("Old Local Gateway interface successfully removed: %s", ifaceID)
+	}
 }

go-controller/pkg/ovn/gateway_init.go
@@ -190,6 +190,10 @@ func gatewayInit(nodeName string, clusterIPSubnet []*net.IPNet, hostSubnets []*n
 			"stderr: %q, error: %v", externalSwitch, stdout, stderr, err)
 	}
 
+	// OCP HACK - clean up old local gateway port that might still be in DB
+	cleanOldLocalGWPort(nodeName)
+	// END OCP HACK
+
 	// Add external interface as a logical port to external_switch.
 	// This is a learning switch port with "unknown" address. The external
 	// world is accessed via this port.